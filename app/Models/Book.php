aa55e6f24c9f3caf8f935542120dec726aed0afd
Kei-K23 <keiksl2301@gmail.com>
2024-09-07 21:02:59 +0630

    Fix query schema

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Book extends Model
 {
@@ -14,8 +14,8 @@ class Book extends Model
         'title',
     ];
 
-    public function books(): HasOne
+    public function author(): BelongsTo
     {
-        return $this->hasOne(Author::class);
+        return $this->belongsTo(Author::class);
     }
 }
